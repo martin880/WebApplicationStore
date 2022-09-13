ded0d53f3d0785dcce0d41ba6298de709458fdf4
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2022-09-13 14:49:29 +0700

    Add project files.

---
+++ WebApplicationStore/Infrastructure/WebApplicationStoreContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationStore.Models;
+
+namespace WebApplicationStore.Infrastructure
+{
+    public class WebApplicationStoreContext : DbContext
+    {
+        public WebApplicationStoreContext(DbContextOptions<WebApplicationStoreContext>options)
+            : base(options)
+        {
+           
+        }
+
+        public DbSet<Page> Pages { get; set; }
+    }
+}
